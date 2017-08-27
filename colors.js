3dd4dcd4cfbe8817eaf70ac824db507a83538259
Kairat <kairat.kemp@gmail.com>
2017-08-26 21:31:24 -0700

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,7 @@
+var player = map.getPlayer();
+var randNumb = Math.floor(Math.random() * (3 - 0));
+var arrayOfColors=['#f00','#0f0','#ff0'];
+player.setColor(arrayOfColors[randNumb]);// no idea how to state of this color made random. Eventually it will hit right color!
+        
+        
+        
